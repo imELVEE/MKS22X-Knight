58aa8b01c8a17b114aac60ff00095bdb5c7080cb
Alvi Aziz <aaziz00@stuy.edu>
2019-02-13 10:48:58 -0500

    made methods for throws and boolean for knight move possibilities

--- KnightBoard.java
+++ KnightBoard.java
@@ -2,9 +2,7 @@ public class KnightBoard{
   private int[][] board;
 
   public KnightBoard(int startingRows, int startingCols){
-    if(startingRows < 0 || startingCols < 0){
-      throw new IllegalArgumentException("PARAMETERS ARE NEGATIVE");
-    }
+    isNegative(startingRows,startingCols);
     board = new int[startingRows][startingCols];
     clear();
   }
@@ -30,5 +28,92 @@ public class KnightBoard{
     }
     return boardString;
   }
+
+  public boolean solve(int startingRows, int startingCols){
+    isNegative(startingRows,startingCols);
+    isEmpty();
+    return solve(startingRows,startingCols,1);
+  }
+
+  private boolean solve(int startingRows, int startingCols, int level){
+    if (level == startingRows * startingCols){
+      return true;
+    }
+    for (int d = 1 ; d < 9 ; d++){
+      System.out.println(canmove(startingRows, startingCols, d));
+    }
+    return false;
+  }
+
+  private boolean canmove(int row, int col, int direction){
+    //remember rows are up and down, cols are left and right
+
+    //up
+    if (direction == 1){
+      if (row - 2 >= 0 && col - 1 >= 0){
+        return true;
+      }
+    }
+    if (direction == 2){
+      if (row - 2 >= 0 && col + 1 < board[row].length){
+        return true;
+      }
+    }
+
+    //right
+    if (direction == 3){
+      if (row - 1 >= 0 && col + 2 < board[row].length){
+        return true;
+      }
+    }
+    if (direction == 4){
+      if (row + 1 < board.length && col + 2 < board[row].length){
+        return true;
+      }
+    }
+
+    //down
+    if (direction == 5){
+      if (row + 2 < board.length && col + 1 < board[row].length){
+        return true;
+      }
+    }
+    if (direction == 6){
+      if (row + 2 < board.length && col - 1 >= 0){
+        return true;
+      }
+    }
+
+    //left
+    if (direction == 7){
+      if (row - 1 >= 0 && col - 2 >= 0){
+        return true;
+      }
+    }
+    if (direction == 8){
+      if (row - 1 >= 0 && col - 2 >= 0){
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private void isEmpty(){
+    for (int r = 0 ; r < board.length ; r++){
+      for (int c = 0 ; c < board[r].length ; c++){
+        if (board[r][c] != 0){
+          throw new IllegalStateException("BOARD IS NOT EMPTY");
+        }
+      }
+    }
+  }
+
+  private void isNegative(int startingRows, int startingCols){
+    if(startingRows < 0 || startingCols < 0){
+      throw new IllegalArgumentException("PARAMETERS ARE NEGATIVE");
+    }
+  }
+
 
 }
--- mydriver.java
+++ mydriver.java
@@ -29,9 +29,17 @@ public class mydriver{
       System.out.println("It worked!");
     }
 
+
     test = new KnightBoard(5,5);
     System.out.println(test);
     test = new KnightBoard(3,5);
     System.out.println(test);
+
+    System.out.println();
+    test = new KnightBoard(8,8);
+    test.solve(3,3);
+    System.out.println();
+    test.solve(0,0);
+    System.out.println();
   }
 }
